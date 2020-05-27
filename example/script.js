27308415fae951a426b7c3b3a39f77ada1063530
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2020-05-27 14:46:42 -0500

    Updated example file

--- example/script.js
+++ example/script.js
@@ -4,14 +4,19 @@
   const generatedCodeOutput = document.getElementById('generatedcode');
   const avatarSizeInput = document.getElementById('avatarsize');
   const avatarSizeVal = document.getElementById('avatarSizeVal');
+  const cellShapeSelect = document.getElementById('cellshape');
+
+  const customRender = (resolution, indexX, indexY) => `M${(indexX * resolution) + (resolution /2)},${indexY * resolution} l${resolution * 0.5} ${resolution} l-${resolution} 0z`;
+
+  let cellShape = cellShapeSelect.value === 'custom' ? customRender : cellShapeSelect.value;
+
 
   let size = parseInt(avatarSizeInput.value, 10);
-  let resolution = Math.floor(avatarSize / size);
   let avatarData = randomAvatarGenerator.generateRandomAvatarData(size);
   avatarSizeVal.innerText = size;
 
   generatedCodeOutput.innerText = avatarData;
-  randomAvatarGenerator.getAvatarFromData(randombox, resolution, avatarData);
+  randombox.innerHTML = randomAvatarGenerator.getAvatarFromData(avatarData, avatarSize, cellShape);
 
   avatarSizeInput.addEventListener('input', (ev) => {
     avatarSizeVal.innerText = ev.srcElement.value;
@@ -20,13 +25,11 @@
   document.getElementById('updateimage').addEventListener('mouseup', (ev) => {
     ev.preventDefault();
     size = parseInt(avatarSizeInput.value, 10);
-    resolution = Math.floor(avatarSize / size);
+    cellShape = cellShapeSelect.value === 'custom' ? customRender : cellShapeSelect.value;
   
     avatarData = randomAvatarGenerator.generateRandomAvatarData(size);
     generatedCodeOutput.innerText = avatarData;
-    randombox.innerHTML = '';
-    randomAvatarGenerator.getAvatarFromData(randombox, resolution, avatarData);
-
+    randombox.innerHTML = randomAvatarGenerator.getAvatarFromData(avatarData, avatarSize, cellShape);
   })
   
 })(document, randomAvatarGenerator);
