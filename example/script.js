8e562a7242e3325b840fcda0fd86973e6afcbea3
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2020-05-28 16:35:31 -0500

    Improved example playground

--- example/script.js
+++ example/script.js
@@ -2,34 +2,84 @@
   const avatarSize = 256;
   const randombox = document.getElementById('randombox');
   const generatedCodeOutput = document.getElementById('generatedcode');
-  const avatarSizeInput = document.getElementById('avatarsize');
-  const avatarSizeVal = document.getElementById('avatarSizeVal');
+  const svgCodeOutput = document.getElementById('svgcode');
+  const avatarCodeInput = document.getElementById('avatarcodeinput');
+  const avatarComplexityInput = document.getElementById('avatarcomplexity');
+  const avatarComplexityVal = document.getElementById('avatarComplexityVal');
   const cellShapeSelect = document.getElementById('cellshape');
+  const svgFileSize = document.getElementById('svgFileSize');
+  
+  const getRenderMethod = (method) => {  
+    switch (method) {
+      case 'custom triangle':
+        return (res, iX, iY) => `M${(iX * res) + (res / 2)},${iY * res} l${res / 2} ${res} l-${res} 0z`;
+        case 'custom diamond':
+          return (res, iX, iY) => `M${(iX * res) + (res / 2)},${iY * res} l${res / 2} ${res / 2} l-${res / 2} ${res / 2} l-${res / 2} -${res / 2}z`;
+      case 'custom heart':
+        return (res, iX, iY) => `M${(iX * res) + (res * (5 / 64))},${(iY * res) + (res / 2)} a${res / 4},${res / 4} 0 1,1 ${res * (3/8)},-${res * (3/8)} a${res / 4},${res / 4} 0 1,1 ${res * (3/8)},${res * (3/8)} l-${res * (3/8)},${res * (3/8)}z`;
+        default:
+          return method;
+    }
+  };
+      
+  function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+    
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+  const byteSize = str => formatBytes(new Blob([str]).size);
 
-  const customRender = (resolution, indexX, indexY) => `M${(indexX * resolution) + (resolution /2)},${indexY * resolution} l${resolution * 0.5} ${resolution} l-${resolution} 0z`;
+  let cellShape = getRenderMethod(cellShapeSelect.value);
 
-  let cellShape = cellShapeSelect.value === 'custom' ? customRender : cellShapeSelect.value;
-
-
-  let size = parseInt(avatarSizeInput.value, 10);
-  let avatarData = randomAvatarGenerator.generateRandomAvatarData(size);
-  avatarSizeVal.innerText = size;
+  let complexity = parseInt(avatarComplexityInput.value, 10);
+  let avatarData = randomAvatarGenerator.generateRandomAvatarData(complexity);
+  avatarComplexityVal.innerText = complexity;
 
   generatedCodeOutput.innerText = avatarData;
   randombox.innerHTML = randomAvatarGenerator.getAvatarFromData(avatarData, avatarSize, cellShape);
+  svgFileSize.innerText = byteSize(randombox.innerHTML);
+  svgCodeOutput.innerText = randombox.innerHTML;
 
-  avatarSizeInput.addEventListener('input', (ev) => {
-    avatarSizeVal.innerText = ev.srcElement.value;
+  avatarComplexityInput.addEventListener('input', (ev) => {
+    avatarComplexityVal.innerText = ev.srcElement.value;
   }, false);
+
+  avatarCodeInput.addEventListener('input', (ev) => {
+    const avatarCode = ev.srcElement.value.trim();
+    if (avatarCode) {
+      avatarComplexityInput.disabled = true;
+    } else {
+      avatarComplexityInput.disabled = false;
+    }
+  });
   
   document.getElementById('updateimage').addEventListener('mouseup', (ev) => {
     ev.preventDefault();
-    size = parseInt(avatarSizeInput.value, 10);
-    cellShape = cellShapeSelect.value === 'custom' ? customRender : cellShapeSelect.value;
-  
-    avatarData = randomAvatarGenerator.generateRandomAvatarData(size);
+    const avatarCode = avatarCodeInput.value.trim();
+
+    if (avatarCode) {
+      avatarData = avatarCode;
+    } else {
+      complexity = parseInt(avatarComplexityInput.value, 10);
+      avatarData = randomAvatarGenerator.generateRandomAvatarData(complexity);
+    }
+    
+    cellShape = getRenderMethod(cellShapeSelect.value);
     generatedCodeOutput.innerText = avatarData;
-    randombox.innerHTML = randomAvatarGenerator.getAvatarFromData(avatarData, avatarSize, cellShape);
+    try {
+      randombox.innerHTML = randomAvatarGenerator.getAvatarFromData(avatarData, avatarSize, cellShape);
+      svgFileSize.innerText = byteSize(randombox.innerHTML);
+      svgCodeOutput.innerText = randombox.innerHTML;
+    } catch (e) {
+      randombox.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><text x="20" y="50" fill="#d40">ERROR: Wrong avatar code</text></svg>';
+      svgCodeOutput.innerText = e.message;
+    }
   })
   
 })(document, randomAvatarGenerator);
